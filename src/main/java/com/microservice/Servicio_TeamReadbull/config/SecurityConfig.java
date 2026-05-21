02e10332f7fa6144bac6530676f7fc863084238f
diegoortiz1008-hash <diegoortiz1008@gmail.com>
2026-05-20 23:12:50 -0500

    Final commit in fix errors

--- src/main/java/com/microservice/Servicio_TeamReadbull/config/SecurityConfig.java
+++ src/main/java/com/microservice/Servicio_TeamReadbull/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(gatewayAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
